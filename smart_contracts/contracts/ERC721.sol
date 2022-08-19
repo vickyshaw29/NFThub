afe80d2acb97badf03940daad276f8e5dd6322f9
Vicky Prasad <prasadvicky231@gmail.com>
2022-08-20 02:10:16 +0530

    done with enumeration and transfer functionality in ERC721

--- smart_contracts/contracts/ERC721.sol
+++ smart_contracts/contracts/ERC721.sol
@@ -3,14 +3,18 @@ pragma solidity ^0.8.0;
 
 contract ERC721{
     event Transfer(address indexed from , address indexed to,uint256 indexed tokenId);
+    //mapping from tokenId to the owner
     mapping(uint=>address)private tokenOwner;
+    //mapping from owner to number of owned tokens
     mapping(address=>uint) private ownedToken;
+    //mapping from tokenId to approved addresses 
+    mapping(uint256=>address) private tokenApprovals;
 
     function _exists(uint256 tokenId)internal view returns(bool){
         address owner =tokenOwner[tokenId];
         return owner!=address(0);
     }
-    function _mint(address to,uint tokenId)internal{
+    function _mint(address to,uint tokenId)internal virtual{
         require(to!=address(0),"Invalid address");
         require(!_exists(tokenId),"token already minted");
         tokenOwner[tokenId]=to;
@@ -21,9 +25,22 @@ contract ERC721{
         require(owner!=address(0),"Invalid owner");
         return ownedToken[owner];
     }
-    function ownerOf(uint256 tokenId)external view returns(address){
+    function ownerOf(uint256 tokenId)public view returns(address){
         address owner = tokenOwner[tokenId];
         require(owner!=address(0),"Invalid owner");
         return owner;
+    }
+
+    //transfer 
+    function _transferFrom(address _from,address _to,uint256 tokenId)internal{
+        require(_to!=address(0),"Invalid address");
+        require(ownerOf(tokenId)==_from,"You dont have tokens to transfer");
+        ownedToken[_from]-=1;
+        ownedToken[_to]+=1;
+        tokenOwner[tokenId]=_to;
+        emit Transfer(_from, _to, tokenId);
+    }
+    function transferFrom(address _from,address _to,uint256 tokenId)public{
+        _transferFrom(_from, _to, tokenId);
     }
 }
